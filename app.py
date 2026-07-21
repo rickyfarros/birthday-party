6e4496b5509171e8e6811715c7870fc3a8487cb3
rickyfarros <rickyfarros1@gmail.com>
2026-07-21 08:24:52 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Rute untuk halaman utama undangan
+@app.route("/undangan")
+def undangan():
+    # Ini akan membaca file index.html di dalam folder 'templates'
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
